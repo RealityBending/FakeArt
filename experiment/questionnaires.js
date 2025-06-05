08ce6c77ab16f4bd1491e2df690c0e1c22dc0fbe
Karla Wall <kw459@sussex.ac.uk>
2025-06-05 22:54:40 +0100

    Add MINT questionnaire.

--- experiment/questionnaires.js
+++ experiment/questionnaires.js
@@ -86,6 +86,175 @@ var questionnaire_phq4 = {
 
 
 
+//----------------------------------------------------------------------------------------------------------------
+// MINT - Multimodal Interoceptive Inventory
+// In process of validation
+// Sum all items belonging to the same dimension to calculate score
+
+const mint_items = [
+    "Sometimes my breathing becomes erratic or shallow and I often don't know why",
+    "I often feel like I can't get enough oxygen by breathing normally",
+    "Sometimes my heart starts racing and I often don't know why",
+    "I sometimes feel like I need to urinate or defecate but when I go to the bathroom I produce less than I expected",
+    "I often feel the need to urinate even when my bladder is not full",
+    "Sometimes I am not sure whether I need to go to the toilet or not (to urinate or defecate)",
+    "In general, my skin is very sensitive",
+    "My skin is susceptible to itchy fabrics and materials",
+    "I can notice even very subtle stimulations to my skin (e.g., very light touches)",
+    "I often only notice how I am breathing when it becomes loud",
+    "I only notice my heart when it is thumping in my chest",
+    "I often only notice how I am breathing when my breathing becomes shallow or irregular",
+    "I don't always feel the need to eat until I am really hungry",
+    "Sometimes I don't realise I was hungry until I ate something ",
+    "I don't always feel the need to drink until I am really thirsty",
+    "During sex or masturbation, I often feel very strong sensations coming from my genital areas",
+    "When I am sexually aroused, I often notice specific sensations in my genital area (e.g., tingling, warmth, wetness, stiffness, pulsations)",
+    "My genital organs are very sensitive to pleasant stimulations",
+    "Being relaxed is a very different bodily feeling compared to other states (e.g., feeling anxious, sexually aroused or after exercise)",
+    "Being sexually aroused is a very different bodily feeling compared to other states (e.g., feeling anxious, relaxed, or after physical exercise)",
+    "Being anxious is a very different bodily feeling compared to other states (e.g., feeling sexually aroused, relaxed or after exercise)",
+    "In general, I am very sensitive to changes in my genital organs",
+    "I can notice even very subtle changes in the state of my genital organs",
+    "I am always very aware of the state of my genital organs, even when I am calm",
+    "I can always accurately feel when I am about to burp",
+    "I can always accurately feel when I am about to fart",
+    "I can always accurately feel when I am about to sneeze",
+    "I always know when I am relaxed",
+    "I always feel in my body if I am relaxed",
+    "My body is always in the same specific state when I am relaxed",
+    "I can notice even very subtle changes in my breathing",
+    "I am always very aware of how I am breathing, even when I am calm",
+    "In general, I am very sensitive to changes in my breathing",
+    "In general, I am very sensitive to changes in my heart rate",
+    "I can notice even very subtle changes in the way my heart beats",
+    "I often notice changes in my heart rate",
+    "I can notice even very subtle changes in what my stomach is doing",
+    "In general, I am very sensitive to what my stomach is doing",
+    "I am always very aware of what my stomach is doing, even when I am calm",
+    "I often check the smell of my armpits",
+    "I often check the smell of my own breath",
+    "I often check the smell of my farts",
+    "In general, I am very aware of the sensations that are happening when I am urinating",
+    "In general, I am very aware of the sensations that are happening when I am defecating",
+    "I often experience a pleasant sensation when relieving myself when urinating or defecating)",
+]
+
+const mint_dimensions = [
+    "mint_1_cardiac_confusion",
+    "mint_2_cardiac_confusion",
+    "mint_3_cardiac_confusion",
+    "mint_4_urointestinal_inaccuracy",
+    "mint_5_urointestinal_inaccuracy",
+    "mint_6_urointestinal_inaccuracy",
+    "mint_7_dermatoception",
+    "mint_8_dermatoception",
+    "mint_9_dermatoception",
+    "mint_10_cardiorespiratory_noticing",
+    "mint_11_cardiorespiratory_noticing",
+    "mint_12_cardiorespiratory_noticing",
+    "mint_13_satiety_noticing",
+    "mint_14_satiety_noticing",
+    "mint_15_satiety_noticing",
+    "mint_16_sexual_arousal_sensitivity",
+    "mint_17_sexual_arousal_sensitivity",
+    "mint_18_sexual_arousal_sensitivity",
+    "mint_19_state_specificity",
+    "mint_20_state_specificity",
+    "mint_21_state_specificity",
+    "mint_22_sexual_organs_sensitivity",
+    "mint_23_sexual_organs_sensitivity",
+    "mint_24_sexual_organs_sensitivity",
+    "mint_25_expulsion_accuracy",
+    "mint_26_expulsion_accuracy",
+    "mint_27_expulsion_accuracy",
+    "mint_28_relaxation_awareness",
+    "mint_29_relaxation_awareness",
+    "mint_30_relaxation_awareness",
+    "mint_31_respiroception",
+    "mint_32_respiroception",
+    "mint_33_respiroception",
+    "mint_34_cardioception",
+    "mint_35_cardioception",
+    "mint_36_cardioception",
+    "mint_37_gastroception",
+    "mint_38_gastroception",
+    "mint_39_gastroception",
+    "mint_40_olfactory_compensation",
+    "mint_41_olfactory_compensation",
+    "mint_42_olfactory_compensation",
+    "mint_43_urointestinal_sensitivity",
+    "mint_44_urointestinal_sensitivity",
+    "mint_45_urointestinal_sensitivity"
+]
+
+function mint_questions(
+    required = true,
+    ticks = ["Disagree", "Agree"],
+    items = mint_items,
+    dimensions = mint_dimensions
+) {
+    // Build survey items
+    var questions = []
+    for (const [index, element] of items.entries()) {
+        q = {
+            title: element,
+            name: dimensions[index],
+            type: "rating",
+            displayMode: "buttons",
+            isRequired: required,
+            minRateDescription: ticks[0],
+            maxRateDescription: ticks[1],
+            rateValues: [0, 1, 2, 3, 4, 5, 6],
+        }
+        questions.push(q)
+    }
+
+    // Randomize order
+    for (let i = questions.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        ;[questions[i], questions[j]] = [questions[j], questions[i]]
+    }
+
+    // Define attention check
+    const attentionCheck = {
+        title: "I can always accurately answer to the extreme left on this question to show that I am reading it",
+        name: "mint_attention_check",
+        type: "rating",
+        displayMode: "buttons",
+        isRequired: required,
+        minRateDescription: ticks[0],
+        maxRateDescription: ticks[1],
+        rateValues: [0, 1, 2, 3, 4, 5, 6],
+    };
+
+    // Add attention check at end
+    questions.push(attentionCheck)
+
+    return [
+        {
+            elements: questions,
+            description:
+                "Please answer the following questions based on how accurately each statement describes you in general.",
+        },
+    ]
+}
+
+var questionnaire_mint = {
+    type: jsPsychSurvey,
+    survey_json: {
+        title: "Interoception",
+        showQuestionNumbers: false,
+        goNextPageAutomatic: true,
+        pages: mint_questions(),
+    },
+    data: {
+        screen: "questionnaire_mint",
+    },
+}
+
+
+
+//---------------------------------------------------------------------------------------------------------------
 /* Attitudes towards AI *==========================*/
 // Beliefs about Artificial Images Technology (BAIT)
 // History:
