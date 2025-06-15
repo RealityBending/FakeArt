7159e32477a795d905b7372e2a93cd9992b170b1
DominiqueMakowski <dom.mak19@gmail.com>
2025-06-15 14:23:07 +0100

    update consent form and comment off datapipe saving

--- experiment/demographics.js
+++ experiment/demographics.js
@@ -1,6 +1,5 @@
 // Full screen
-var fullscreen_text =
-    "<p>The experiment will switch to full screen mode when you press the button below</p>"
+var fullscreen_text = "<p>The experiment will switch to full screen mode when you press the button below</p>"
 var fullscreen_button = "Continue"
 
 var fullscreen_on = {
@@ -44,35 +43,41 @@ var demographics_browser_info = {
 }
 
 // Consent form ========================================================
-var demographics_consent = {
-    type: jsPsychHtmlButtonResponse,
-    css_classes: ["narrow-text"],
-    stimulus: function () {
+const demographics_consent = {
+    type: jsPsychSurvey,
+    survey_json: function () {
+        // Get URL variables
+        let urlvars = jsPsych.data.urlVariables()
+
         // Logo and title
         let text =
             "<img src='https://blogs.brighton.ac.uk/sussexwrites/files/2019/06/University-of-Sussex-logo-transparent.png' width='150px' align='right'/><br><br><br><br><br>" +
             "<h1>Informed Consent</h1>"
 
-        if (jsPsych.data.urlVariables()["exp"] == "surveyswap") {
+        // this part is about specific messages for participants based on the recruitment platform
+        if (urlvars["exp"] == "surveyswap") {
             text +=
-                "<p style='color:green;' align='left'><b>Note: You will receive a <i style='color:purple;'>SurveySwap.io</i> completion code at the end of the experiment.</b></p>"
+                "<p style='color:green;' align='left'><b>Note: You will receive a <i style='color:purple;'>SurveySwap.io completion code</i>  at the end of the experiment.</b></p>"
         }
 
-        if (jsPsych.data.urlVariables()["exp"] == "SONA") {
+        if (jsPsych.data.urlVariables()["exp"] == "prolific") {
             text +=
-                "<p style='color:green;' align='left'><b>Note: You will receive a <i style='color:purple;'>SONA</i> completion link at the end of the experiment.</b></p>"
+                "<p style='color:green;' align='left'><b>Note: You will receive information regarding your <i style='color:purple;'>Prolific</i> participation at the end of the experiment.</b></p>"
         }
 
-        // Consent Form
+        if (jsPsych.data.urlVariables()["exp"] == "sona") {
+            text +=
+                "<p style='color:green;' align='left'><b>Note: You will receive a <i style='color:purple;'>Sona completion code</i> at the end of the experiment.</b></p>"
+        }
+        // Main Text
         text +=
             // Overview
             "<p align='left'><b>Invitation to Take Part</b><br>" +
-            "Thank you for considering to take part in this study that aims at deepening our understanding of human perception. " +
-            "This study is being conducted by Ana Neves and Dr Dominique Makowski from the <b>University of Sussex</b>, who are happy to be contacted if you have any questions (see contact information below).</p>" +
+            "Thank you for considering to take part in this study conducted by Dr Dominique Makowski from the University of Sussex and his team (see contact information below).</p>" +
             // Description
             "<p align='left'><b>Why have I been invited and what will I do?</b><br>" +
-            "The goal is to study how new technology can impact <b>human perception</b>. In this study, you will be shown images of paintings and asked to complete a few questionnaires and perform some tasks. " +
-            "The whole experiment will take you <b style='color:green;'>~35 min</b> to complete. Please make you sure that you are <b>attentive and in a quiet environment</b>, and that you have time to complete it in one go.</p>" +
+            "The goal is to study how <b>new technology impacts human appreciation of art</b>. In this study, you will be shown images of paintings and asked to complete a few questionnaires. " +
+            "The whole experiment will take you <b style='color:#FF5722;'>~30 min</b> to complete. Please make you sure that you are <b>attentive and in a quiet environment</b>, and that you have time to complete it in one go.</p>" +
             // Results and personal information
             "<p align='left'><b>What will happen to the results and my personal information?</b><br>" +
             "The results of this research may be written into a scientific publication. Your anonymity will be ensured in the way described in the consent information below. <b>Please read this information carefully</b> and then, if you wish to take part, please acknowledge that you have fully understood this sheet, and that you consent to take part in the study as it is described here.</p>" +
@@ -82,17 +87,36 @@ var demographics_consent = {
             "<li align='left'>I understand that my participation is entirely voluntary, that I can choose not to participate in part or all of the study, and that I can withdraw at any stage without having to give a reason and without being penalized in any way (e.g., if I am a student, my decision whether or not to take part will not affect my grades).</li>" +
             "<li align='left'>I understand that since the study is anonymous, it will be impossible to withdraw my data once I have completed it.</li>" +
             "<li align='left'>I understand that my personal data will be used for the purposes of this research study and will be handled in accordance with Data Protection legislation. I understand that the University's Privacy Notice provides further information on how the University uses personal data in its research.</li>" +
-            "<li align='left'>I understand that my collected data will be stored in a de-identified way. De-identified data may be made publicly available through secured scientific online data repositories.</li>" +
-            // Incentive
-            "<li align='left'>Please note that various checks will be performed to ensure the validity of the data. We reserve the right to withhold credit awards or reimbursement should we detect non-valid responses (e.g., random patterns of answers, instructions not read, ...).</li>" +
-            "<li align='left'>By participating, you agree to follow the instructions and provide honest answers. If you do not wish to participate, simply close your browser.</li>" +
-            "</p>" +
-            "<p align='left'><br><sub><sup>For further information about this research, or if you have any concerns, please contact Ana Neves (<i style='color:DodgerBlue;'>A.Neves@sussex.ac.uk</i>) and/or Dr Dominique Makowski (<i style='color:DodgerBlue;'>D.Makowski@sussex.ac.uk</i>). This research has been approved (ER/ST633/1) by the ethics board of the School of Psychology. The University of Sussex has insurance in place to cover its legal liabilities in respect of this study.</sup></sub></p>"
+            "<li align='left'>I understand that my collected data will be stored in a de-identified way. De-identified data may be made publicly available through secured scientific online data repositories.</li>"
 
-        return text
+        // Incentive
+        if (["surveyswap", "prolific", "sona"].includes(urlvars["exp"])) {
+            text +=
+                "<li align='left'>Please note that <b style='color:#FF5722;'>various checks will be performed to ensure the validity of the data</b>. We reserve the right to withhold credit awards or reimbursement should we detect non-valid responses (e.g., random patterns of answers, instructions not read, ...).</li>"
+        }
+
+        // End
+        text +=
+            "<li align='left'>By participating, you agree to follow the instructions and provide honest answers. If you do not wish to participate or if you don't have the time, simply close your browser.</li></p>" +
+            "<p align='left'><br><sub><sup>For further information about this research, or if you have any concerns, please contact Dr Dominique Makowski (<i style='color:DodgerBlue;'>D.Makowski@sussex.ac.uk</i>) and/or Ana Neves (<i style='color:DodgerBlue;'>A.Neves@sussex.ac.uk</i>). This research has been approved (ER/EB672/2) by the Sciences & Technology Cross-Schools Research Ethics Committee (C-REC) (<i style='color:DodgerBlue;'>crecscitec@sussex.ac.uk</i>). The University of Sussex has insurance in place to cover its legal liabilities in respect of this study.</sup></sub></p>"
+
+        // Return Survey
+        return {
+            showQuestionNumbers: false,
+            completeText: "I read, understood, and I consent",
+            pages: [
+                {
+                    elements: [
+                        {
+                            type: "html",
+                            name: "ConsentForm",
+                            html: text,
+                        },
+                    ],
+                },
+            ],
+        }
     },
-    choices: ["I read, understood, and I consent"],
-    data: { screen: "consent" },
 }
 
 // Demographic info ========================================================================
@@ -112,7 +136,10 @@ var demographics_questions = {
                         title: "What is your gender?",
                         name: "Gender",
                         type: "radiogroup",
-                        choices: ["Male", "Female", "Other"],
+                        choices: ["Male", "Female"],
+                        showOtherItem: true,
+                        otherText: "Other",
+                        otherPlaceholder: "Please specify",
                         isRequired: true,
                         colCount: 0,
                     },
@@ -163,8 +190,7 @@ var demographics_questions = {
                         colCount: 1,
                     },
                     {
-                        visibleIf:
-                            "{Education} == 'Doctorate' || {Education} == 'Master' || {Education} == 'Bachelor'",
+                        visibleIf: "{Education} == 'Doctorate' || {Education} == 'Master' || {Education} == 'Bachelor'",
                         title: "What is your discipline?",
                         name: "Discipline",
                         type: "radiogroup",
@@ -186,8 +212,7 @@ var demographics_questions = {
                         otherPlaceholder: "Please specify",
                     },
                     {
-                        visibleIf:
-                            "{Education} == 'High school' || {Education} == 'Master' || {Education} == 'Bachelor'",
+                        visibleIf: "{Education} == 'High school' || {Education} == 'Master' || {Education} == 'Bachelor'",
                         title: "Are you currently a student?",
                         name: "Student",
                         type: "boolean",
@@ -231,7 +256,6 @@ var demographics_questions = {
                     },
                 ],
             },
-           
         ],
     },
     data: {
