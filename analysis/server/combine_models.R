6e546b91ceedfed0e22e1df6b598c14110583c48
DominiqueMakowski <dom.mak19@gmail.com>
2026-06-27 22:52:45 +0100

    fixes

--- analysis/server/combine_models.R
+++ analysis/server/combine_models.R
@@ -8,27 +8,29 @@ library(loo)
 options(mc.cores = as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK")))
 task_id <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
 
-setwd("/mnt/lustre/users/psych/dmm56/FictionChoco/")
+
+setwd("/mnt/lustre/scratch/psych/dmm56/FakeArt/")
 
 # List of models
-model_names <- c("sample1_zoib", "sample1_betagate", "sample1_choco",
-                 "sample1_chocoattractiveness", "sample1_chocobeauty",
-                 "sample2_chocoattractiveness", "sample2_betagate",
-                 "sample2noex_betagate", "sample2noex_chocoattractiveness", "sample2noex_chocobeauty")[4:10]
+model_names <- c("Beauty")
 
 # Select the model name based on the task ID
 model_name <- model_names[task_id]
 
 combine_and_save <- function(name) {
   print(paste0(name, ": ", Sys.time()))
-  files <- list.files(".", pattern = paste0(name, '_.*rds$'), full.names = TRUE)
+  files <- list.files("models/", pattern = paste0(name, "_.*rds$"), full.names = TRUE)
+  print(paste0("Found ", length(files), " files for model: ", name))
   m <- brms::combine_models(mlist = lapply(files, readRDS))
 
   # Add criterion and save
   m <- brms::add_criterion(m, "waic", ndraws = 1500, file = name)
 
-  # saveRDS(m, paste0(name, ".rds"))
+  # Save combined model
+  saveRDS(m, paste0(name, ".rds"))
+
+  print(paste0("Finished: ", name, " at ", Sys.time()))
 }
 
-combine_and_save(model_name)
+combine_and_save(model_names)
 print(paste0("Completed: ", Sys.time()))
