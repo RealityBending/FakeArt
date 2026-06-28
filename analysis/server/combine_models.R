71a234812d2a762d6f37c472c3fd9d283023a7df
DominiqueMakowski <dom.mak19@gmail.com>
2026-06-28 22:58:29 +0100

    update server

--- analysis/server/combine_models.R
+++ analysis/server/combine_models.R
@@ -6,21 +6,21 @@ library(loo)
 
 # Get the number of cores and task ID from the environment variables
 options(mc.cores = as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK")))
-task_id <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+task_id <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID", unset = "1"))
 
 
 setwd("/mnt/lustre/scratch/psych/dmm56/FakeArt/")
 
 # List of models
-model_names <- c("Beauty")
+model_names <- c("Beauty", "Reality")
 
 # Select the model name based on the task ID
-model_name <- model_names[task_id]
+name <- model_names[task_id]
 
 combine_and_save <- function(name) {
-  print(paste0(name, ": ", Sys.time()))
+  print(paste0(" **", name, ": ", Sys.time()))
   files <- list.files("models/", pattern = paste0(name, "_.*rds$"), full.names = TRUE)
-  print(paste0("Found ", length(files), " files for model: ", name))
+  print(paste0("** Found ", length(files), " files for model: ", name))
   m <- brms::combine_models(mlist = lapply(files, readRDS))
 
   # Add criterion and save
@@ -29,8 +29,8 @@ combine_and_save <- function(name) {
   # Save combined model
   saveRDS(m, paste0(name, ".rds"))
 
-  print(paste0("Finished: ", name, " at ", Sys.time()))
+  print(paste0("** Finished: ", name, " at ", Sys.time()))
 }
 
-combine_and_save(model_names)
-print(paste0("Completed: ", Sys.time()))
+combine_and_save(name)
+print(paste0("** Completed: ", Sys.time()))
--- analysis/server/make_models.R
+++ analysis/server/make_models.R
@@ -3,11 +3,21 @@ library(cmdstanr)
 library(cogmod)
 
 task_id <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID", unset = "1"))
-chains_per_task <- as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK", unset = "2"))
-start_chain <- (task_id - 1) * chains_per_task + 1
+total_cores <- as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK", unset = "2"))
+
+cat("================\nNode Info:")
+cat(paste0("** Task ID: ", task_id, "\n"))
+cat(paste0("** Total cores: ", total_cores, "\n"))
+cat("** GCC version: \n")
+system("g++ --version")
+
+# Hardcode the chains for this node, and calculate threads dynamically
+chains_per_node <- 2
+threads_per_chain <- total_cores / chains_per_node # 16 cores / 2 chains = 8 threads per chain
+
 warmup <- 1200
-iter <- warmup + 600
-seed <- 1234 + start_chain
+iter <- warmup + 400
+seed <- 1234 + task_id
 
 
 path <- "/mnt/lustre/scratch/psych/dmm56/FakeArt/models/"
@@ -20,43 +30,93 @@ path <- "/mnt/lustre/scratch/psych/dmm56/FakeArt/models/"
 dftask <- read.csv("https://raw.githubusercontent.com/RealityBending/FakeArt/refs/heads/main/data/data_task.csv")
 dftask$Condition <- factor(dftask$Condition, levels = c("Human Original", "Human Forgery", "AI-Generated"))
 
+
+priors <- c(
+  brms::set_prior("normal(0, 2)", class = "Intercept", dpar = ""),
+  brms::set_prior("normal(0, 2)", class = "Intercept", dpar = "confright"),
+  brms::set_prior("normal(0, 2)", class = "Intercept", dpar = "confleft"),
+  brms::set_prior("normal(0, 3)", class = "Intercept", dpar = "precright"),
+  brms::set_prior("normal(0, 3)", class = "Intercept", dpar = "precleft"),
+  brms::set_prior("normal(0, 3)", class = "Intercept", dpar = "pex"),
+  brms::set_prior("normal(0, 3)", class = "Intercept", dpar = "bex"),
+  brms::set_prior("normal(0, 3)", class = "Intercept", dpar = "pmid"),
+  brms::set_prior("normal(0, 1)", class = "b", coef = "", dpar = ""),
+  brms::set_prior("normal(0, 1)", class = "b", coef = "", dpar = "confright"),
+  brms::set_prior("normal(0, 1)", class = "b", coef = "", dpar = "confleft"),
+  brms::set_prior("normal(0, 1)", class = "b", coef = "", dpar = "precright"),
+  brms::set_prior("normal(0, 1)", class = "b", coef = "", dpar = "precleft"),
+  brms::set_prior("normal(0, 1)", class = "b", coef = "", dpar = "pex"),
+  brms::set_prior("normal(0, 1)", class = "b", coef = "", dpar = "bex"),
+  brms::set_prior("normal(0, 1)", class = "b", coef = "", dpar = "pmid")
+)
+
 # ----------------------------
 # Beauty
 # ----------------------------
 
 
 formula <- brms::bf(
-  Beauty ~ Condition + (1 | Participant) + (1 | Item),
-  confright ~ Condition + (1 | Participant) + (1 | Item),
-  confleft ~ Condition + (1 | Participant) + (1 | Item),
+  Beauty ~ Condition + (Condition | Participant) + (Condition | Item),
+  confright ~ Condition + (Condition | Participant) + (Condition | Item),
+  confleft ~ Condition + (Condition | Participant) + (Condition | Item),
   precright ~ Condition + (1 | Participant) + (1 | Item),
   precleft ~ Condition + (1 | Participant) + (1 | Item),
-  pex ~ 1 + (1 | Participant),
-  bex ~ 1 + (1 | Participant),
-  pmid ~ 1 + (1 | Participant),
+  pex ~ Condition + (1 | Participant),
+  bex ~ Condition + (1 | Participant),
+  pmid ~ Condition + (1 | Participant),
   family = choco()
 )
 
-priors <- c(
-  brms::set_prior("normal(0, 3)", class = "Intercept", dpar = ""),
-  brms::set_prior("normal(0, 3)", class = "Intercept", dpar = "confright"),
-  brms::set_prior("normal(0, 3)", class = "Intercept", dpar = "confleft"),
-  brms::set_prior("normal(0, 3)", class = "Intercept", dpar = "precright"),
-  brms::set_prior("normal(0, 3)", class = "Intercept", dpar = "precleft"),
-  brms::set_prior("normal(0, 3)", class = "Intercept", dpar = "pex"),
-  brms::set_prior("normal(0, 3)", class = "Intercept", dpar = "bex"),
-  brms::set_prior("normal(0, 3)", class = "Intercept", dpar = "pmid")
-) |>
-  brms::validate_prior(formula, data = dftask)
 
-m_choco <- brm(formula,
-  data = dftask, family = choco(), stanvars = choco_stanvars(),
-  prior = priors, init = 0,
-  chains = chains_per_task,
-  cores = chains_per_task,
+m_beauty <- brm(formula,
+  data = dftask,
+  family = choco(), stanvars = choco_stanvars(),
+  prior = brms::validate_prior(priors, formula, data = dftask),
+  init = 0,
+  chains = chains_per_node,
+  cores = chains_per_node,
+  threads = threading(threads_per_chain),
   iter = iter,
   warmup = warmup,
-  seed = 1234 + start_chain,
+  seed = seed,
   backend = "cmdstanr",
   file = paste0(path, "Beauty_task_", task_id)
 )
+
+print("!! BEAUTY DONE !!")
+
+
+# ----------------------------
+# Reality
+# ----------------------------
+
+
+formula <- brms::bf(
+  Reality ~ Condition + (Condition | Participant) + (Condition | Item),
+  confright ~ Condition + (Condition | Participant) + (Condition | Item),
+  confleft ~ Condition + (Condition | Participant) + (Condition | Item),
+  precright ~ Condition + (1 | Participant) + (1 | Item),
+  precleft ~ Condition + (1 | Participant) + (1 | Item),
+  pex ~ Condition + (1 | Participant),
+  bex ~ Condition + (1 | Participant),
+  pmid ~ Condition + (1 | Participant),
+  family = choco()
+)
+
+
+m_reality <- brm(formula,
+  data = dftask[!is.na(dftask$Reality), ],
+  family = choco(), stanvars = choco_stanvars(),
+  prior = brms::validate_prior(priors, formula, data = dftask[!is.na(dftask$Reality), ]),
+  init = 0,
+  chains = chains_per_node,
+  cores = chains_per_node,
+  threads = threading(threads_per_chain),
+  iter = iter,
+  warmup = warmup,
+  seed = seed,
+  backend = "cmdstanr",
+  file = paste0(path, "Reality_task_", task_id)
+)
+
+print("!! REALITY DONE !!")
